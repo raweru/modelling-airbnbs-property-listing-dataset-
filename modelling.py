88e4030f8301769444712f51d1f1082bf9d29436
raweru <dominik.dekleva@hotmail.com>
2023-07-08 13:23:04 +0100

    M4T5: add save_model

--- modelling.py
+++ modelling.py
@@ -9,21 +9,26 @@ import joblib
 import json
 
 
+# Load the tabular data using load_airbnb function
+features, labels = load_airbnb(label="Price_Night")
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+X_test, X_valid, y_test, y_valid = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+
+y_train = y_train.values.ravel()  # Convert y_train to 1-dimensional array
+y_valid = y_valid.values.ravel()  # Convert y_valid to 1-dimensional array
+y_test = y_test.values.ravel()  # Convert y_test to 1-dimensional array
+
+
 def train_linear_regression():
-    # Load the tabular data using load_airbnb function
-    features, labels = load_airbnb(label="Price_Night")
-
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=4)
-    X_test, X_valid, y_test, y_valid = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
-
-    y_train = y_train.values.ravel()  # Convert y_train to 1-dimensional array
-    y_valid = y_valid.values.ravel()  # Convert y_valid to 1-dimensional array
-    y_test = y_test.values.ravel()  # Convert y_test to 1-dimensional array
+    
+    metrics = {}
     
     # Initialize and train the linear regression model
     model = SGDRegressor()
     model.fit(X_train, y_train)
+    hyperparameters = model.get_params()
 
     # Compute predictions for the training set
     train_predictions = model.predict(X_train)
@@ -32,27 +37,32 @@ def train_linear_regression():
     test_predictions = model.predict(X_test)
 
     # Compute RMSE for the training set
-    train_rmse = np.sqrt(mean_squared_error(y_train, train_predictions))
+    metrics["train_RMSE"] = np.sqrt(mean_squared_error(y_train, train_predictions))
 
     # Compute RMSE for the test set
-    test_rmse = np.sqrt(mean_squared_error(y_test, test_predictions))
+    metrics["test_RMSE"] = np.sqrt(mean_squared_error(y_test, test_predictions))
 
     # Compute R^2 score for the training set
-    train_r2 = r2_score(y_train, train_predictions)
+    metrics["train_R2"] = r2_score(y_train, train_predictions)
 
     # Compute R^2 score for the test set
-    test_r2 = r2_score(y_test, test_predictions)
+    metrics["test_R2"] = r2_score(y_test, test_predictions)
 
     # Print the performance measures
     print("--------------------------------")
-    print("BASELINE MODEL (NO FINE TUNING)")
-    print("Training set RMSE: ", train_rmse)
-    print("Test set RMSE: ", test_rmse)
-    print("Training set R^2: ", train_r2)
-    print("Test set R^2: ", test_r2)
+    print("BASELINE MODEL, NO FINE TUNING")
+    print(f"Model:{model}")
+    print("")
+    print("Hyperparameters:")
+    for param, value in hyperparameters.items():
+        print(f"{param}: {value}")
+    print("")
+    print("Metrics:")
+    for param, value in metrics.items():
+        print(f"{param}: {value}")
     print("--------------------------------")
     
-    return X_train, y_train, X_valid, y_valid, X_test, y_test
+    return model, hyperparameters, metrics
 
 
 def generate_parameter_combinations(hyperparameters):
@@ -62,7 +72,7 @@ def generate_parameter_combinations(hyperparameters):
         yield dict(zip(keys, combination))
 
 
-def custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, X_valid, y_valid, X_test, y_test, hyperparameters):
+def custom_tune_regression_model_hyperparameters(model_class, hyperparameters):
     best_model = None
     best_params = {}
     best_metrics = {"validation_RMSE": float('inf')}
@@ -99,22 +109,28 @@ def custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, 
     
     print("--------------------------------")
     print("CUSTOM GRID SEARCH")
-    print(f"Best model:{best_model}")
-    print(f"Best params:{best_params}")
-    print(f"Best metrics:{best_metrics}")
+    print(f"Best Model:{best_model}")
+    print("")
+    print("Best Hyperparameters:")
+    for param, value in best_params.items():
+        print(f"{param}: {value}")
+    print("")
+    print("Best Metrics:")
+    for param, value in best_metrics.items():
+        print(f"{param}: {value}")
     print("--------------------------------")
     
     return best_model, best_params, best_metrics
 
 
-def tune_regression_model_hyperparameters(X_train, y_train, X_test, y_test):
+def tune_regression_model_hyperparameters():
     
     best_metrics = {}
     
     model = SGDRegressor()
     
     param_grid = {
-        'loss': ['squared_loss', 'huber'],
+        'loss': ['squared_error', 'huber'],
         'penalty': ['l2', 'l1', 'elasticnet'],
         'alpha': [0.0001, 0.001, 0.01],
         'learning_rate': ['constant', 'optimal', 'invscaling'],
@@ -140,83 +156,77 @@ def tune_regression_model_hyperparameters(X_train, y_train, X_test, y_test):
     best_metrics["test_R2"] = best_model.score(X_test, y_test)
     
     print("--------------------------------")
-    print("SKLEARN GRIDSEARCHCV")
+    print("SKLEARN GRID SEARCH")
+    print(f"Best Model:{best_model}")
+    print("")
     print("Best Hyperparameters:")
-    print(best_params)
-    print(f"Best Metrics: {best_metrics}")
+    for param, value in best_params.items():
+        print(f"{param}: {value}")
+    print("")
+    print("Best Metrics:")
+    for param, value in best_metrics.items():
+        print(f"{param}: {value}")
     print("--------------------------------")
     
     return best_model, best_params, best_metrics
 
 
-def save_model(model, folder):
+def save_model(model, hyperparameters, metrics, folder, model_filename, params_filename, metrics_filename):
     # Create the directory if it doesn't exist
     os.makedirs(folder, exist_ok=True)
 
     # Save the model
-    model_path = os.path.join(folder, "model.joblib")
+    model_path = os.path.join(folder, f"{model_filename}.joblib")
     joblib.dump(model, model_path)
 
-    # Extract hyperparameters from the model
-    hyperparameters = model.get_params()
-
     # Save the hyperparameters
-    hyperparameters_path = os.path.join(folder, "hyperparameters.json")
+    hyperparameters_path = os.path.join(folder, f"{params_filename}.json")
     with open(hyperparameters_path, "w") as file:
         json.dump(hyperparameters, file)
 
-    # Calculate and extract metrics from the model
-    
-    mse = mean_squared_error(y_true, y_pred)
-    rmse = np.sqrt(mse)
-    r2 = r2_score(y_true, y_pred)
-
-    metrics = {
-        "RMSE": rmse,
-        "R2": r2
-    }
-
     # Save the metrics
-    metrics_path = os.path.join(folder, "metrics.json")
+    metrics_path = os.path.join(folder, f"{metrics_filename}.json")
     with open(metrics_path, "w") as file:
         json.dump(metrics, file)
 
 
 
-if __name__ == "__main__":
-    
-    #TODO: Train baseline model
-    X_train, y_train, X_valid, y_valid, X_test, y_test = train_linear_regression()
+def main(models_to_run):
 
-    """Training set RMSE:  1158344774.6662142
-    Test set RMSE:  1228675273.35997
-    Training set R^2:  -74626863578953.1
-    Test set R^2:  -111151183104622.05"""
+    if 'baseline' in models_to_run:
+        
+        baseline_model, baseline_params, baseline_metrics = train_linear_regression()
+        
+        save_model(baseline_model, baseline_params, baseline_metrics, 'models/regression/linear_regression/baseline_sgd/',
+                   'baseline_sgd_model', 'baseline_sgd_params', 'baseline_sgd_metrics')
 
-    #TODO: Custom fine tune grid search
-    # Define the hyperparameters to be tuned
-    hyperparameters = {
-        'loss': ['squared_loss', 'huber'],
+    if 'custom' in models_to_run:
+        
+        custom_model_class = SGDRegressor
+        
+        custom_hyperparameters = {
+        'loss': ['squared_error', 'huber'],
         'penalty': ['l2', 'l1', 'elasticnet'],
         'alpha': [0.0001, 0.001, 0.01],
         'learning_rate': ['constant', 'optimal', 'invscaling'],
         'max_iter': [1000, 2000],
         'early_stopping': [True, False],
         'validation_fraction': [0.1, 0.2],
-        'tol': [1e-3, 1e-4]
-    }
-
-    # Create an instance of the model class (e.g., SGDRegressor)
-    model_class = SGDRegressor
-
-    # Call the custom grid search function
-    best_model, best_params, best_metrics = custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, X_valid, y_valid, X_test, y_test, hyperparameters)
-
-    """ Best model:SGDRegressor(alpha=0.1, learning_rate='adaptive')
-        Best params:{'alpha': 0.1, 'learning_rate': 'adaptive'}
-        Best metrics:{'validation_RMSE': 143459528.71772546, 'train_RMSE': 164706481.42072955, 
-        'test_RMSE': 146281798.80221358, 'train_R2': -1508831981312.8716, 'test_R2': -1295221330677.812}"""
-
-    #TODO: sklearn fine tune grid search
+        'tol': [1e-3, 1e-4]}
+        
+        custom_model, custom_params, custom_metrics = custom_tune_regression_model_hyperparameters(
+            custom_model_class, custom_hyperparameters)
+        
+        save_model(custom_model, custom_params, custom_metrics, 'models/regression/linear_regression/custom_sgd/',
+                   'custom_sgd_model', 'custom_sgd_params', 'custom_sgd_metrics')
     
-    tune_regression_model_hyperparameters(X_train, y_train, X_test, y_test)
+    if 'sklearn' in models_to_run:
+        
+        sklearn_model, sklearn_params, sklearn_metrics = tune_regression_model_hyperparameters()
+        
+        save_model(sklearn_model, sklearn_params, sklearn_metrics, 'models/regression/linear_regression/sklearn_sgd/',
+                    'sklearn_sgd_model', 'sklearn_sgd_params', 'sklearn_sgd_metrics')
+
+
+if __name__ == "__main__":
+    main(['baseline', 'custom', 'sklearn'])
